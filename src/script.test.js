13f8a88c3f0be190529eb959747d4669ee2e6daf
Ana Scolari <127357173+apsscolari@users.noreply.github.com>
2025-06-05 15:25:35 -0700

    Update script.test.js

--- src/script.test.js
+++ src/script.test.js
@@ -1,1 +1,9 @@
 alert('test script to be excluded')
+const http = require('http');
+const url = require('url');
+
+http.createServer(function (req, res) {
+    const query = url.parse(req.url, true).query;
+    // Vulnerable to reflected XSS
+    res.end('<h1>Hello, ' + query.name + '!</h1>');
+}).listen(8080);
